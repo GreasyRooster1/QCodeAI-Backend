fc732ba659b9ea17be314a899002999291f2f76c
Dillon Wilson <greasyRooster1@gmail.com>
2026-07-08 11:20:15 -0400

    fix cors

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from app.api.routes import ai, combo_game, prompts, nn, pet_trainer
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "https://ai.esporterz.com"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET","POST","OPTIONS"],
     allow_headers=["*"],
 )
 
