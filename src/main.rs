fbf831f0542c24a06cb26fbcb5024e520a1b9a79
GreasyRooster <greasyRooster1@gmail.com>
2026-08-05 20:07:54 -0700

    cors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use] extern crate rocket;
 
 use std::sync::Arc;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::{Request, Response};
+use rocket::http::Header;
 use rocket::serde::json::Json;
 use rocket::serde::{Deserialize, Serialize};
 use rocket_firebase_auth::{FirebaseAuth, FirebaseToken};
@@ -23,7 +26,7 @@ async fn rocket() -> _ {
         .unwrap();
 
     rocket::build()
-    .mount("/",routes![status])
+    .mount("/",routes![status,all_options])
     .mount("/api", routes![
         generate
     ]).manage(ServerState { auth: firebase_auth })
@@ -39,4 +42,28 @@ fn generate(token: FirebaseToken) -> Result<Json<AIResponse>, String> {
     Ok(Json(AIResponse {
         output: "success".to_string(),
     }))
+}
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "http://localhost:3000, ai.esporterz.com"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+#[options("/<_..>")]
+fn all_options() {
+    /* Intentionally left empty */
 }
