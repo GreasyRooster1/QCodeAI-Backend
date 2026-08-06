edd50a3deb2a21672fb2eb23950c576cf5f41176
GreasyRooster <greasyRooster1@gmail.com>
2026-08-05 19:55:19 -0700

    firebase setup

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 #[macro_use] extern crate rocket;
 
+use std::sync::Arc;
+use firebase_auth::{FirebaseAuth, FirebaseAuthState, FirebaseUser};
+
 
 
 #[launch]
-fn rocket() -> _ {
+async fn rocket() -> _ {
+
+    let project_id = "your-firebase-project-id";
+    let firebase_auth = FirebaseAuth::new(project_id).await;
+
+    // 2. Wrap it in an Arc to share safely across threads
+    let auth_state = FirebaseAuthState {
+        firebase_auth: Arc::new(firebase_auth),
+    };
+
     rocket::build().mount("/", routes![
         status
     ])
 }
 
 #[get("/")]
-fn status() -> &str {
+fn status() -> &'static str {
     "running"
 }
